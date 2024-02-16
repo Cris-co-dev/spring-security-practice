4e9b6d8c77deb09f0b25cc8bfebce5520be8a840
Cris <corsamps@gmail.com>
2024-02-16 15:33:07 -0500

    Initial commit

---
+++ src/test/java/com/crisdev/api/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisdev.api.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
