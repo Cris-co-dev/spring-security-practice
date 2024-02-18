b335ca37f85bebf637b5c59a8318fa0971560ec0
Cris <corsamps@gmail.com>
2024-02-17 21:15:29 -0500

    Updated UserService

--- src/main/java/com/crisdev/api/springsecuritypractice/service/UserService.java
+++ src/main/java/com/crisdev/api/springsecuritypractice/service/UserService.java
@@ -3,6 +3,11 @@ package com.crisdev.api.springsecuritypractice.service;
 import com.crisdev.api.springsecuritypractice.dto.SaveUser;
 import com.crisdev.api.springsecuritypractice.persistence.entity.User;
 
+import java.util.Map;
+import java.util.Optional;
+
 public interface UserService {
     User registerOneCustomer(SaveUser newUser);
+
+    Optional<User> findOneByUsername(String username);
 }
